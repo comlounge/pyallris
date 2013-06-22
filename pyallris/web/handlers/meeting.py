f43a81e3e5f363cc95185dc9513308b8161bd292
Christian Scholz <cs@comlounge.net>
2013-06-22 16:17:59 +0200

    printing transcripts on meeting page

--- pyallris/web/handlers/meeting.py
+++ pyallris/web/handlers/meeting.py
@@ -5,7 +5,18 @@ class Meeting(Handler):
     template = "meeting.html"
 
     def get(self, silfdnr):
+
         meeting = self.app.mongodb.meetings.find_one({'_id': int(silfdnr) } )
-        return self.render(meeting = meeting)
+
+        tolfdnrs = []
+        for top in meeting['tops']:
+            tolfdnrs.append(top['tolfdnr'])
+
+        top_infos_raw = self.app.mongodb.agenda_items.find({'_id': {'$in' : tolfdnrs}})
+        top_infos = {} 
+        for top in top_infos_raw:
+            top_infos[top['_id']] = top
+
+        return self.render(meeting = meeting, top_infos = top_infos)
 
 
